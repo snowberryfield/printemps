c7d974a1026fdd4022d385e3f3cd07fdad4ff542
snowberryfield <yuji.koguma@gmailcom>
2020-06-10 21:20:07 +0900

    Refactor console_utility.h.

--- cppmh/utility/console_utility.h
+++ cppmh/utility/console_utility.h
@@ -15,9 +15,8 @@ inline void print_single_line(const bool a_IS_ENABLED) {
     if (!a_IS_ENABLED) {
         return;
     }
-    std::cout << "_____________________________________________________________"
-                 "___________________"
-              << std::endl;
+    std::string single_line(80, '_');
+    std::cout << single_line << std::endl;
 }
 
 /******************************************************************************/
@@ -25,9 +24,8 @@ inline void print_single_broken_line(const bool a_IS_ENABLED) {
     if (!a_IS_ENABLED) {
         return;
     }
-    std::cout << "-------------------------------------------------------------"
-                 "-------------------"
-              << std::endl;
+    std::string single_broken_line(80, '-');
+    std::cout << single_broken_line << std::endl;
 }
 
 /******************************************************************************/
@@ -35,18 +33,17 @@ inline void print_double_line(const bool a_IS_ENABLED) {
     if (!a_IS_ENABLED) {
         return;
     }
-    std::cout << "============================================================="
-                 "==================="
-              << std::endl;
+    std::string double_line(80, '=');
+    std::cout << double_line << std::endl;
 }
 
 /******************************************************************************/
-inline void print_line_break(int        a_number_of_line_breaks,
+inline void print_line_break(int        a_NUMBER_OF_LINE_BREAKS,
                              const bool a_IS_ENABLED) {
     if (!a_IS_ENABLED) {
         return;
     }
-    for (auto i = 0; i < a_number_of_line_breaks; i++) {
+    for (auto i = 0; i < a_NUMBER_OF_LINE_BREAKS; i++) {
         std::cout << std::endl;
     }
 }
@@ -90,125 +87,53 @@ inline void print_parameter(const std::string &a_NAME, const double a_VALUE,
 }
 
 /******************************************************************************/
-inline void print_parameter(const std::string &a_NAME, const int a_VALUE,
-                            const bool a_IS_ENABLED) {
+inline void print_with_label(const std::string &a_HEADER_LABEL,
+                             const std::string &a_MESSAGE,
+                             const bool         a_IS_ENABLED) {
     if (!a_IS_ENABLED) {
         return;
     }
-    char temp[80];
-    sprintf(temp, "%25s : %d", a_NAME.c_str(), a_VALUE);
-    std::cout << temp << std::endl;
-}
+    const std::string LABEL_FIRST_ROW = a_HEADER_LABEL;
+    const std::string LABEL_OTHER_ROW(LABEL_FIRST_ROW.size(), ' ');
 
-/******************************************************************************/
-inline void print_parameter(const std::string &a_NAME,
-                            const std::string  a_VALUE,
-                            const bool         a_IS_ENABLED) {
-    if (!a_IS_ENABLED) {
-        return;
-    }
-    char temp[80];
-    sprintf(temp, "%25s : %s", a_NAME.c_str(), a_VALUE.c_str());
-    std::cout << temp << std::endl;
-}
+    const int NUMBER_OF_CHARACTERS_PER_LINE = 80 - LABEL_FIRST_ROW.size();
 
-/******************************************************************************/
-inline void print_parameter(const std::string &a_NAME, const double a_BEGIN,
-                            const double a_END, const bool a_IS_ENABLED) {
-    if (!a_IS_ENABLED) {
-        return;
-    }
-    char temp[80];
-    sprintf(temp, "%25s : %.6e -> %.6e", a_NAME.c_str(), a_BEGIN, a_END);
-    std::cout << temp << std::endl;
-}
-
-/******************************************************************************/
-inline void print_message(const std::string &a_LINE, const bool a_IS_ENABLED) {
-    if (!a_IS_ENABLED) {
-        return;
-    }
-    const std::string str = "MESSAGE : " + a_LINE;
-
-    constexpr int NUMBER_OF_CHARACTERS_PER_LINE = 80;
-
-    int number_of_lines = (str.size() - 1) / NUMBER_OF_CHARACTERS_PER_LINE + 1;
+    int number_of_lines =
+        (a_MESSAGE.size() - 1) / NUMBER_OF_CHARACTERS_PER_LINE + 1;
     for (auto i = 0; i < number_of_lines; i++) {
+        std::string label = (i == 0) ? LABEL_FIRST_ROW : LABEL_OTHER_ROW;
         if (i == number_of_lines - 1) {
-            std::cout << str.substr(i * NUMBER_OF_CHARACTERS_PER_LINE)
+            std::cout << label
+                      << a_MESSAGE.substr(i * NUMBER_OF_CHARACTERS_PER_LINE)
                       << std::endl;
         } else {
-            std::cout << str.substr(i * NUMBER_OF_CHARACTERS_PER_LINE,
-                                    NUMBER_OF_CHARACTERS_PER_LINE)
+            std::cout << label
+                      << a_MESSAGE.substr(i * NUMBER_OF_CHARACTERS_PER_LINE,
+                                          NUMBER_OF_CHARACTERS_PER_LINE)
                       << std::endl;
         }
     }
 }
-
 /******************************************************************************/
-inline void print_error(const std::string &a_LINE, const bool a_IS_ENABLED) {
-    if (!a_IS_ENABLED) {
-        return;
-    }
-    const std::string str = "ERROR   : " + a_LINE;
-
-    constexpr int NUMBER_OF_CHARACTERS_PER_LINE = 80;
-
-    int number_of_lines = (str.size() - 1) / NUMBER_OF_CHARACTERS_PER_LINE + 1;
-    for (auto i = 0; i < number_of_lines; i++) {
-        if (i == number_of_lines - 1) {
-            std::cout << str.substr(i * NUMBER_OF_CHARACTERS_PER_LINE)
-                      << std::endl;
-        } else {
-            std::cout << str.substr(i * NUMBER_OF_CHARACTERS_PER_LINE,
-                                    NUMBER_OF_CHARACTERS_PER_LINE)
-                      << std::endl;
-        }
-    }
+inline void print_message(const std::string &a_MESSAGE,
+                          const bool         a_IS_ENABLED) {
+    print_with_label("MESSAGE : ", a_MESSAGE, a_IS_ENABLED);
 }
 
 /******************************************************************************/
-inline void print_warning(const std::string &a_LINE, const bool a_IS_ENABLED) {
-    if (!a_IS_ENABLED) {
-        return;
-    }
-    const std::string str = "WARNING : " + a_LINE;
-
-    constexpr int NUMBER_OF_CHARACTERS_PER_LINE = 80;
-
-    int number_of_lines = (str.size() - 1) / NUMBER_OF_CHARACTERS_PER_LINE + 1;
-    for (auto i = 0; i < number_of_lines; i++) {
-        if (i == number_of_lines - 1) {
-            std::cout << str.substr(i * NUMBER_OF_CHARACTERS_PER_LINE)
-                      << std::endl;
-        } else {
-            std::cout << str.substr(i * NUMBER_OF_CHARACTERS_PER_LINE,
-                                    NUMBER_OF_CHARACTERS_PER_LINE)
-                      << std::endl;
-        }
-    }
+inline void print_error(const std::string &a_MESSAGE, const bool a_IS_ENABLED) {
+    print_with_label("ERROR   : ", a_MESSAGE, a_IS_ENABLED);
 }
 
 /******************************************************************************/
-inline void print_info(const std::string &a_LINE, const bool a_IS_ENABLED) {
-    if (!a_IS_ENABLED) {
-        return;
-    }
-    const std::string str = "INFO    : " + a_LINE;
+inline void print_warning(const std::string &a_MESSAGE,
+                          const bool         a_IS_ENABLED) {
+    print_with_label("WARNING : ", a_MESSAGE, a_IS_ENABLED);
+}
 
-    constexpr int NUMBER_OF_CHARACTERS_PER_LINE = 80;
-
-    int number_of_lines = (str.size() - 1) / NUMBER_OF_CHARACTERS_PER_LINE + 1;
-    for (auto i = 0; i < number_of_lines; i++) {
-        if (i == number_of_lines - 1) {
-            std::cout << str.substr(i * NUMBER_OF_CHARACTERS_PER_LINE)
-                      << std::endl;
-        } else {
-            std::cout << str.substr(i * NUMBER_OF_CHARACTERS_PER_LINE,
-                                    NUMBER_OF_CHARACTERS_PER_LINE)
-                      << std::endl;
-        }
-    }
+/******************************************************************************/
+inline void print_info(const std::string &a_MESSAGE, const bool a_IS_ENABLED) {
+    print_with_label("INFO    : ", a_MESSAGE, a_IS_ENABLED);
 }
 }  // namespace utility
 }  // namespace cppmh
