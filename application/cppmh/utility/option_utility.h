03565f8c62f688f0fd96cfb2bec1dd72326ba441
snowberryfield <yuji.koguma@gmailcom>
2020-06-14 11:52:41 +0900

    Add the option "is_enabled_presolve" to the JSON option reader.

--- application/cppmh/utility/option_utility.h
+++ application/cppmh/utility/option_utility.h
@@ -83,6 +83,12 @@ inline cppmh::solver::Option read_option(const std::string &a_FILE_NAME) {
               option_object);
 
     /**************************************************************************/
+    /// is_enabled_presolve
+    read_json(&option.is_enabled_presolve,  //
+              "is_enabled_presolve",        //
+              option_object);
+
+    /**************************************************************************/
     /// is_enabled_initial_value_correction
     read_json(&option.is_enabled_initial_value_correction,  //
               "is_enabled_initial_value_correction",        //
