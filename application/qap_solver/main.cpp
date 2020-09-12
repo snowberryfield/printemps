2cd696f86b87e5266a3a02c89951528af5e7e7a2
snowberryfield <yuji.koguma@gmail.com>
2020-09-12 22:33:43 +0900

    Turn the default verbose settings of mps_solver and qap_solver to None.

--- application/qap_solver/main.cpp
+++ application/qap_solver/main.cpp
@@ -54,7 +54,6 @@ int main([[maybe_unused]] int argc, char *argv[]) {
      * be used for the calculation. Otherwise, the default values will be used.
      */
     cppmh::solver::Option option;
-    option.verbose = cppmh::solver::Full;
     if (!option_file_name.empty()) {
         option = cppmh::utility::read_option(option_file_name);
     }
@@ -72,10 +71,12 @@ int main([[maybe_unused]] int argc, char *argv[]) {
      * Print the result summary.
      */
     cppmh::utility::print_info(
-        "status: " + std::to_string(result.solution.is_feasible()), true);
+        "status: " + std::to_string(result.solution.is_feasible()),
+        option.verbose != cppmh::solver::Verbose::None);
 
     cppmh::utility::print_info(
-        "objective: " + std::to_string(result.solution.objective()), true);
+        "objective: " + std::to_string(result.solution.objective()),
+        option.verbose != cppmh::solver::Verbose::None);
 
     result.solution.write_json_by_name("incumbent.json");
     result.solution.write_solution("incumbent.sol");
