f683c2d4918d8947aec1bf33dcef654b43b38964
snowberryfield <yuji.koguma@gmailcom>
2020-10-24 00:00:53 +0900

    Implement termination status enumerations for lagrange_dual, local_search and tabu_search.

--- cppmh/solver/tabu_search/tabu_search_result.h
+++ cppmh/solver/tabu_search/tabu_search_result.h
@@ -14,19 +14,29 @@ class IncumbentHolder;
 
 namespace tabu_search {
 /*****************************************************************************/
+enum class TabuSearchTerminationStatus {
+    TIME_OVER,
+    ITERATION_OVER,
+    NO_MOVE,
+    REACH_TARGET,
+    EARLY_STOP
+};
+
+/*****************************************************************************/
 template <class T_Variable, class T_Expression>
 struct TabuSearchResult {
     IncumbentHolder<T_Variable, T_Expression> incumbent_holder;
     Memory                                    memory;
-    int                                       tabu_tenure;
-    int                                       total_update_status;
-    int                                       number_of_iterations;
+
+    int tabu_tenure;
+    int total_update_status;
+    int number_of_iterations;
 
     int last_local_augmented_incumbent_update_iteration;
     int last_global_augmented_incumbent_update_iteration;
     int last_feasible_incumbent_update_iteration;
 
-    bool is_early_stopped;
+    TabuSearchTerminationStatus termination_status;
 
     std::vector<model::PlainSolution<T_Variable, T_Expression>>
         historical_feasible_solutions;
@@ -45,6 +55,7 @@ struct TabuSearchResult {
     inline void constexpr initialize(void) {
         this->incumbent_holder.initialize();
         this->memory.initialize();
+
         this->tabu_tenure          = 0;
         this->total_update_status  = 0;
         this->number_of_iterations = 0;
@@ -53,7 +64,7 @@ struct TabuSearchResult {
         this->last_global_augmented_incumbent_update_iteration = -1;
         this->last_feasible_incumbent_update_iteration         = -1;
 
-        this->is_early_stopped = false;
+        this->termination_status = TabuSearchTerminationStatus::ITERATION_OVER;
 
         this->historical_feasible_solutions.clear();
     }
