685d18d27053da713c22b2cc3e189ffa6b61e926
snowberryfield <yuji.koguma@gmailcom>
2021-01-29 00:33:34 +0900

    Output: Add version number of the program to the output files.

--- printemps/constant.h
+++ printemps/constant.h
@@ -6,6 +6,7 @@
 #ifndef PRINTEMPS_CONSTANT_H__
 #define PRINTEMPS_CONSTANT_H__
 
+#include <string>
 #include <limits>
 
 namespace printemps {
@@ -13,6 +14,7 @@ namespace constant {
 static constexpr auto EPSILON      = 1E-5;
 static constexpr auto INT_HALF_MIN = std::numeric_limits<int>::min() / 2 + 1;
 static constexpr auto INT_HALF_MAX = (std::numeric_limits<int>::max() - 1) / 2;
+static std::string    VERSION      = "v1.5.3";
 }  // namespace constant
 }  // namespace printemps
 
