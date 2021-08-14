2471f1b4dff8732ee4c05dd2e3fd7f8e1e7057cb
snowberryfield <yuji.koguma@gmail.com>
2021-08-14 13:52:57 +0900

    Feature: Implement a utility class BidirectionalMap.

---
+++ printemps/utility/bidirectional_map.h
@@ -0,0 +1,62 @@
+/*****************************************************************************/
+// Copyright (c) 2020 Yuji KOGUMA
+// Released under the MIT license
+// https://opensource.org/licenses/mit-license.php
+/*****************************************************************************/
+#ifndef PRINTEMPS_UTILITY_BIDIRECTIONAL_MAP_H__
+#define PRINTEMPS_UTILITY_BIDIRECTIONAL_MAP_H__
+
+namespace printemps {
+namespace utility {
+/*****************************************************************************/
+template <class T1, class T2>
+class BidirectionalMap {
+   private:
+    std::unordered_map<T1, T2> m_forward;
+    std::unordered_map<T2, T1> m_reverse;
+
+   public:
+    /*************************************************************************/
+    BidirectionalMap(void) {
+        this->initialize();
+    }
+
+    /*************************************************************************/
+    inline void initialize(void) {
+        m_forward.clear();
+        m_reverse.clear();
+    }
+
+    /*************************************************************************/
+    inline constexpr int size(void) const {
+        return m_forward.size();
+    }
+
+    /*************************************************************************/
+    inline constexpr bool empty(void) const {
+        return m_forward.empty() && m_reverse.empty();
+    }
+
+    /*************************************************************************/
+    inline constexpr void insert(const T1 &a_FIRST, const T2 &a_SECOND) {
+        m_forward[a_FIRST]  = a_SECOND;
+        m_reverse[a_SECOND] = a_FIRST;
+    }
+
+    /*************************************************************************/
+    inline constexpr T2 &operator[](const T1 a_KEY) {
+        return m_forward[a_KEY];
+    }
+
+    /*************************************************************************/
+    inline constexpr T1 &operator[](const T2 a_KEY) {
+        return m_reverse[a_KEY];
+    }
+};
+}  // namespace utility
+}  // namespace printemps
+
+#endif
+/*****************************************************************************/
+// END
+/*****************************************************************************/
---
+++ test/utility/test_bidirectional_map.cpp
@@ -0,0 +1,61 @@
+/*****************************************************************************/
+// Copyright (c) 2020 Yuji KOGUMA
+// Released under the MIT license
+// https://opensource.org/licenses/mit-license.php
+/*****************************************************************************/
+#include <gtest/gtest.h>
+#include <string>
+
+#include <printemps.h>
+
+namespace {
+/*****************************************************************************/
+class TestBidirectionalMap : public ::testing::Test {
+   protected:
+    virtual void SetUp(void) {
+        /// nothing to do
+    }
+    virtual void TearDown() {
+        /// nothing to do
+    }
+};
+
+/*****************************************************************************/
+TEST_F(TestBidirectionalMap, initialize) {
+    printemps::utility::BidirectionalMap<int, std::string> bmap;
+
+    EXPECT_TRUE(bmap.empty());
+}
+
+/*****************************************************************************/
+TEST_F(TestBidirectionalMap, operator_square_bracket) {
+    printemps::utility::BidirectionalMap<int, std::string> bmap;
+
+    bmap.insert(0, "zero");
+    bmap.insert(1, "one");
+    bmap.insert(10, "ten");
+
+    EXPECT_EQ("zero", bmap[0]);
+    EXPECT_EQ("one", bmap[1]);
+    EXPECT_EQ("ten", bmap[10]);
+
+    EXPECT_EQ(0, bmap["zero"]);
+    EXPECT_EQ(1, bmap["one"]);
+    EXPECT_EQ(10, bmap["ten"]);
+
+    EXPECT_EQ(3, bmap.size());
+}
+
+/*****************************************************************************/
+TEST_F(TestBidirectionalMap, empty) {
+    /// This method is tested in initialize().
+}
+
+/*****************************************************************************/
+TEST_F(TestBidirectionalMap, size) {
+    /// This method is tested in operator_square_bracket().
+}
+}  // namespace
+/*****************************************************************************/
+// END
+/*****************************************************************************/
