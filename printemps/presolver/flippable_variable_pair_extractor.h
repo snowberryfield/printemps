c372d9e366d14bdf40cfa009464abaca623af3b1
snowberryfield <yuji.koguma@gmailcom>
2022-02-22 23:37:51 +0900

    Refactor: Integrate the functions of extracting flippable variable pairs into the newly created class FlippableVariablePairExtractor.

--- printemps/presolver/flippable_variable_pair_extractor.h
+++ printemps/presolver/flippable_variable_pair_extractor.h
@@ -16,6 +16,13 @@ struct FlippableVariablePair {
     int    number_of_common_element;
     double overlap_rate;
 
+    /*************************************************************************/
+    FlippableVariablePair(void) {
+        this->initialize();
+        /// nothing to do
+    }
+
+    /*************************************************************************/
     FlippableVariablePair(  //
         model_component::Variable<T_Variable, T_Expression>
             *a_variable_ptr_first,
@@ -29,104 +36,147 @@ struct FlippableVariablePair {
           overlap_rate(a_OVERLAP_RATE) {
         /// nothing to do
     }
+
+    /*************************************************************************/
+    void initialize(void) {
+        this->variable_ptr_first       = nullptr;
+        this->variable_ptr_second      = nullptr;
+        this->number_of_common_element = 0;
+        this->overlap_rate             = 0;
+    }
 };
-
 /*****************************************************************************/
 template <class T_Variable, class T_Expression>
-inline void write_flippable_variable_pairs(
-    const std::vector<FlippableVariablePair<T_Variable, T_Expression> >
-        &              a_FLIPPABLE_VARIABLE_PAIRS,
-    const std::string &a_FILE_NAME) {
-    std::ofstream ofs(a_FILE_NAME);
-    for (const auto &pair : a_FLIPPABLE_VARIABLE_PAIRS) {
-        ofs << pair.variable_ptr_first->name() << " "
-            << pair.variable_ptr_second->name() << " "
-            << pair.variable_ptr_first->related_constraint_ptrs().size() << " "
-            << pair.variable_ptr_second->related_constraint_ptrs().size() << " "
-            << pair.number_of_common_element << " " << pair.overlap_rate << " "
-            << std::endl;
+class FlippableVariablePairExtractor {
+   private:
+    model::Model<T_Variable, T_Expression> *                     m_model_ptr;
+    std::vector<FlippableVariablePair<T_Variable, T_Expression>> m_pairs;
+
+   public:
+    /*************************************************************************/
+    FlippableVariablePairExtractor(void) {
+        this->initialize();
     }
-    ofs.close();
-}
 
-/*****************************************************************************/
-template <class T_Variable, class T_Expression>
-std::vector<FlippableVariablePair<T_Variable, T_Expression> >
-extract_flippable_variable_pairs(
-    const std::vector<model_component::Constraint<T_Variable, T_Expression> *>
-        &      a_CONSTRAINT_PTRS,         //
-    const int  a_MINIMUM_COMMON_ELEMENT,  //
-    const bool a_IS_ENABLED_PRINT) {
-    utility::print_single_line(a_IS_ENABLED_PRINT);
-    utility::print_message("Extracting flippable variable pairs...",
-                           a_IS_ENABLED_PRINT);
+    /*************************************************************************/
+    FlippableVariablePairExtractor(
+        model::Model<T_Variable, T_Expression> *a_model_ptr) {
+        this->setup(a_model_ptr);
+    }
 
-    std::unordered_set<model_component::Variable<T_Variable, T_Expression> *>
-        variable_ptrs_set;
-    for (const auto &constraint_ptr : a_CONSTRAINT_PTRS) {
-        for (const auto &sensitivity :
-             constraint_ptr->expression().sensitivities()) {
-            auto variable_ptr = sensitivity.first;
-            if (!variable_ptr->is_fixed() &&
-                (variable_ptr->sense() ==
-                     model_component::VariableSense::Binary ||
-                 variable_ptr->sense() ==
-                     model_component::VariableSense::Selection) &&
-                static_cast<int>(
-                    variable_ptr->related_constraint_ptrs().size()) >=
-                    a_MINIMUM_COMMON_ELEMENT) {
-                variable_ptrs_set.insert(variable_ptr);
+    /*************************************************************************/
+    inline void setup(model::Model<T_Variable, T_Expression> *a_model_ptr) {
+        this->initialize();
+        this->m_model_ptr = a_model_ptr;
+    }
+
+    /*************************************************************************/
+    inline void initialize(void) {
+        m_model_ptr = nullptr;
+        m_pairs.clear();
+    }
+
+    /*************************************************************************/
+    inline void extract_pairs(const int  a_MINIMUM_COMMON_ELEMENT,  //
+                              const bool a_IS_ENABLED_PRINT) {
+        utility::print_single_line(a_IS_ENABLED_PRINT);
+        utility::print_message("Extracting flippable variable pairs...",
+                               a_IS_ENABLED_PRINT);
+
+        const auto &a_CONSTRAINT_PTRS =
+            m_model_ptr->constraint_reference().enabled_constraint_ptrs;
+
+        std::unordered_set<
+            model_component::Variable<T_Variable, T_Expression> *>
+            variable_ptrs_set;
+        for (const auto &constraint_ptr : a_CONSTRAINT_PTRS) {
+            for (const auto &sensitivity :
+                 constraint_ptr->expression().sensitivities()) {
+                auto variable_ptr = sensitivity.first;
+                if (!variable_ptr->is_fixed() &&
+                    (variable_ptr->sense() ==
+                         model_component::VariableSense::Binary ||
+                     variable_ptr->sense() ==
+                         model_component::VariableSense::Selection) &&
+                    static_cast<int>(
+                        variable_ptr->related_constraint_ptrs().size()) >=
+                        a_MINIMUM_COMMON_ELEMENT) {
+                    variable_ptrs_set.insert(variable_ptr);
+                }
             }
         }
-    }
-    auto      variable_ptrs  = utility::to_vector(variable_ptrs_set);
-    const int VARIABLES_SIZE = variable_ptrs.size();
+        auto      variable_ptrs  = utility::to_vector(variable_ptrs_set);
+        const int VARIABLES_SIZE = variable_ptrs.size();
 
-    std::vector<FlippableVariablePair<T_Variable, T_Expression> >
-        flippable_variable_pairs;
-    flippable_variable_pairs.reserve(VARIABLES_SIZE);
+        std::vector<FlippableVariablePair<T_Variable, T_Expression>> pairs;
+        pairs.reserve(VARIABLES_SIZE);
 
-    for (auto i = 0; i < VARIABLES_SIZE; i++) {
-        auto &constraint_ptrs_first =
-            variable_ptrs[i]->related_constraint_ptrs();
-        for (auto j = i + 1; j < VARIABLES_SIZE; j++) {
-            auto &constraint_ptrs_second =
-                variable_ptrs[j]->related_constraint_ptrs();
-            auto intersection_set = utility::intersection_set(
-                constraint_ptrs_first, constraint_ptrs_second);
-            auto union_set = utility::union_set(constraint_ptrs_first,
-                                                constraint_ptrs_second);
+        for (auto i = 0; i < VARIABLES_SIZE; i++) {
+            auto &constraint_ptrs_first =
+                variable_ptrs[i]->related_constraint_ptrs();
+            for (auto j = i + 1; j < VARIABLES_SIZE; j++) {
+                auto &constraint_ptrs_second =
+                    variable_ptrs[j]->related_constraint_ptrs();
+                auto intersection_set = utility::intersection_set(
+                    constraint_ptrs_first, constraint_ptrs_second);
+                auto union_set = utility::union_set(constraint_ptrs_first,
+                                                    constraint_ptrs_second);
 
-            int    number_of_common_elements = intersection_set.size();
-            double overlap_rate =
-                static_cast<double>(number_of_common_elements) /
-                static_cast<double>(union_set.size());
+                int    number_of_common_elements = intersection_set.size();
+                double overlap_rate =
+                    static_cast<double>(number_of_common_elements) /
+                    static_cast<double>(union_set.size());
 
-            if (number_of_common_elements >= a_MINIMUM_COMMON_ELEMENT) {
-                flippable_variable_pairs.emplace_back(
-                    variable_ptrs[i], variable_ptrs[j],
-                    number_of_common_elements, overlap_rate);
-                utility::print_message(
-                    "A flippable variable pair (" + variable_ptrs[i]->name() +
-                        ", " + variable_ptrs[j]->name() + ")" + " was found " +
-                        "(" + std::to_string(number_of_common_elements) + "/" +
-                        std::to_string(overlap_rate) + ").",
-                    a_IS_ENABLED_PRINT);
+                if (number_of_common_elements >= a_MINIMUM_COMMON_ELEMENT) {
+                    pairs.emplace_back(variable_ptrs[i], variable_ptrs[j],
+                                       number_of_common_elements, overlap_rate);
+                    utility::print_message(
+                        "A flippable variable pair (" +
+                            variable_ptrs[i]->name() + ", " +
+                            variable_ptrs[j]->name() + ")" + " was found " +
+                            "(" + std::to_string(number_of_common_elements) +
+                            "/" + std::to_string(overlap_rate) + ").",
+                        a_IS_ENABLED_PRINT);
+                }
             }
         }
+        std::sort(pairs.begin(), pairs.end(),
+                  [](const auto &a_FIRST, const auto &a_SECOND) {
+                      return a_FIRST.overlap_rate > a_SECOND.overlap_rate;
+                  });
+
+        utility::print_message(std::to_string(pairs.size()) +
+                                   " flippable variable pairs were found.",
+                               a_IS_ENABLED_PRINT);
+        utility::print_message("Done.", a_IS_ENABLED_PRINT);
+
+        m_pairs = pairs;
     }
-    std::sort(flippable_variable_pairs.begin(), flippable_variable_pairs.end(),
-              [](const auto &a_FIRST, const auto &a_SECOND) {
-                  return a_FIRST.overlap_rate > a_SECOND.overlap_rate;
-              });
-    utility::print_message(std::to_string(flippable_variable_pairs.size()) +
-                               " flippable variable pairs were found.",
-                           a_IS_ENABLED_PRINT);
 
-    utility::print_message("Done.", a_IS_ENABLED_PRINT);
-    return flippable_variable_pairs;
-}
+    /*************************************************************************/
+    inline void write_pairs(const std::string &a_FILE_NAME) const {
+        std::ofstream ofs(a_FILE_NAME);
+        for (const auto &pair : m_pairs) {
+            ofs << pair.variable_ptr_first->name() << " "
+                << pair.variable_ptr_second->name() << " "
+                << pair.variable_ptr_first->related_constraint_ptrs().size()
+                << " "
+                << pair.variable_ptr_second->related_constraint_ptrs().size()
+                << " " << pair.number_of_common_element << " "
+                << pair.overlap_rate << " " << std::endl;
+        }
+        ofs.close();
+    }
 
+    /*************************************************************************/
+    inline constexpr const std::vector<
+        FlippableVariablePair<T_Variable, T_Expression>>
+        &pairs(void) const {
+        return this->m_pairs;
+    }
+};
+using IPFlippableVariablePairExtractor =
+    FlippableVariablePairExtractor<int, double>;
 }  // namespace presolver
 }  // namespace printemps
 #endif
