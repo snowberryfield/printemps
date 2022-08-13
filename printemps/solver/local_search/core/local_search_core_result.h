df7394ab28b4dbe558d94c384271e121fbc341f4
snowberryfield <yuji.koguma@gmailcom>
2022-08-13 16:24:14 +0900

    Change: Add elapsed time as member of search result structs.

--- printemps/solver/local_search/core/local_search_core_result.h
+++ printemps/solver/local_search/core/local_search_core_result.h
@@ -10,8 +10,9 @@ namespace printemps::solver::local_search::core {
 /*****************************************************************************/
 template <class T_Variable, class T_Expression>
 struct LocalSearchCoreResult {
-    int total_update_status;
-    int number_of_iterations;
+    int    total_update_status;
+    int    number_of_iterations;
+    double elapsed_time;
 
     LocalSearchCoreTerminationStatus termination_status;
 
@@ -25,6 +26,7 @@ struct LocalSearchCoreResult {
         const LocalSearchCoreState<T_Variable, T_Expression> &a_STATE)
         : total_update_status(a_STATE.total_update_status),
           number_of_iterations(a_STATE.iteration),
+          elapsed_time(a_STATE.elapsed_time),
           termination_status(a_STATE.termination_status) {
         /// nothing to do
     }
@@ -33,6 +35,7 @@ struct LocalSearchCoreResult {
     void initialize(void) {
         this->total_update_status  = 0;
         this->number_of_iterations = 0;
+        this->elapsed_time         = 0.0;
         this->termination_status =
             LocalSearchCoreTerminationStatus::ITERATION_OVER;
     }
