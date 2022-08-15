87939f7921eb401a140afc54ef465229caa8e2d9
snowberryfield <yuji.koguma@gmailcom>
2022-08-15 13:18:42 +0900

    Option: Change the default values of neighborhood.is_enabled_exclusive_or(nor)_move from true to false.

--- printemps/option/neighborhood_option.h
+++ printemps/option/neighborhood_option.h
@@ -11,8 +11,8 @@ namespace printemps::option {
 struct NeighborhoodOptionConstant {
     static constexpr bool DEFAULT_IS_ENABLED_BINARY_MOVE                = true;
     static constexpr bool DEFAULT_IS_ENABLED_INTEGER_MOVE               = true;
-    static constexpr bool DEFAULT_IS_ENABLED_EXCLUSIVE_OR_MOVE          = true;
-    static constexpr bool DEFAULT_IS_ENABLED_EXCLUSIVE_NOR_MOVE         = true;
+    static constexpr bool DEFAULT_IS_ENABLED_EXCLUSIVE_OR_MOVE          = false;
+    static constexpr bool DEFAULT_IS_ENABLED_EXCLUSIVE_NOR_MOVE         = false;
     static constexpr bool DEFAULT_IS_ENABLED_INVERTED_INTEGERS_MOVE     = true;
     static constexpr bool DEFAULT_IS_ENABLED_BALANCED_INTEGERS_MOVE     = true;
     static constexpr bool DEFAULT_IS_ENABLED_CONSTANT_SUM_INTEGERS_MOVE = true;
