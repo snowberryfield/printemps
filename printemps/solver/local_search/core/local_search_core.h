2a20aff29e572a47ac1d2513edf61964b6e51490
snowberryfield <yuji.koguma@gmailcom>
2024-03-31 00:01:31 +0900

    License: Update the license description.

--- printemps/solver/local_search/core/local_search_core.h
+++ printemps/solver/local_search/core/local_search_core.h
@@ -1,5 +1,5 @@
 /*****************************************************************************/
-// Copyright (c) 2020-2023 Yuji KOGUMA
+// Copyright (c) 2020-2024 Yuji KOGUMA
 // Released under the MIT license
 // https://opensource.org/licenses/mit-license.php
 /*****************************************************************************/
