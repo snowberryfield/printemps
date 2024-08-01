cbf079a67cf2a3afb920db95569ce3ad5ea77671
snowberryfield <yuji.koguma@gmailcom>
2024-08-01 23:52:59 +0900

    Change: Enable the evaluator for selection moves in the local search.

--- printemps/solver/local_search/core/local_search_core.h
+++ printemps/solver/local_search/core/local_search_core.h
@@ -553,6 +553,12 @@ class LocalSearchCore {
                     m_model_ptr->evaluate_single(&trial_solution_scores[i],  //
                                                  *TRIAL_MOVE_PTRS[i],        //
                                                  CURRENT_SOLUTION_SCORE);
+                } else if (TRIAL_MOVE_PTRS[i]->is_selection_move) {
+                    m_model_ptr->evaluate_selection(  //
+                        &trial_solution_scores[i],    //
+                        *TRIAL_MOVE_PTRS[i],          //
+                        CURRENT_SOLUTION_SCORE);
+
                 } else {
                     m_model_ptr->evaluate_multi(    //
                         &trial_solution_scores[i],  //
