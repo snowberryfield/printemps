a0e1be157f5c49bbc632d9d8d3e8f714c454d236
snowberryfield <yuji.koguma@gmailcom>
2024-09-07 21:20:54 +0900

    Feature: Implement a utility function from_vector() for std::unordered_set.

--- test/utility/test_unordered_set_utility.cpp
+++ test/utility/test_unordered_set_utility.cpp
@@ -84,33 +84,6 @@ TEST_F(TestUnorderedSetUtility, union_set) {
     std::unordered_set<std::string> set_string_0 = {"a", "b", "c", "d", "e"};
     std::unordered_set<std::string> set_string_1 = {"c", "d", "e", "f", "g"};
 
-    utility::update_union_set(&set_int_0, set_int_1);
-    utility::update_union_set(&set_string_0, set_string_1);
-
-    EXPECT_TRUE(set_int_0.find(1) != set_int_0.end());
-    EXPECT_TRUE(set_int_0.find(2) != set_int_0.end());
-    EXPECT_TRUE(set_int_0.find(3) != set_int_0.end());
-    EXPECT_TRUE(set_int_0.find(4) != set_int_0.end());
-    EXPECT_TRUE(set_int_0.find(5) != set_int_0.end());
-    EXPECT_TRUE(set_int_0.find(6) != set_int_0.end());
-    EXPECT_TRUE(set_int_0.find(7) != set_int_0.end());
-
-    EXPECT_TRUE(set_string_0.find("a") != set_string_0.end());
-    EXPECT_TRUE(set_string_0.find("b") != set_string_0.end());
-    EXPECT_TRUE(set_string_0.find("c") != set_string_0.end());
-    EXPECT_TRUE(set_string_0.find("d") != set_string_0.end());
-    EXPECT_TRUE(set_string_0.find("e") != set_string_0.end());
-    EXPECT_TRUE(set_string_0.find("f") != set_string_0.end());
-    EXPECT_TRUE(set_string_0.find("g") != set_string_0.end());
-}
-
-/*****************************************************************************/
-TEST_F(TestUnorderedSetUtility, update_union_set) {
-    std::unordered_set<int>         set_int_0    = {1, 2, 3, 4, 5};
-    std::unordered_set<int>         set_int_1    = {3, 4, 5, 6, 7};
-    std::unordered_set<std::string> set_string_0 = {"a", "b", "c", "d", "e"};
-    std::unordered_set<std::string> set_string_1 = {"c", "d", "e", "f", "g"};
-
     auto union_int = utility::union_set(set_int_0, set_int_1);
     auto union_str = utility::union_set(set_string_0, set_string_1);
 
@@ -129,6 +102,33 @@ TEST_F(TestUnorderedSetUtility, update_union_set) {
     EXPECT_TRUE(union_str.find("e") != union_str.end());
     EXPECT_TRUE(union_str.find("f") != union_str.end());
     EXPECT_TRUE(union_str.find("g") != union_str.end());
+}
+
+/*****************************************************************************/
+TEST_F(TestUnorderedSetUtility, update_union_set) {
+    std::unordered_set<int>         set_int_0    = {1, 2, 3, 4, 5};
+    std::unordered_set<int>         set_int_1    = {3, 4, 5, 6, 7};
+    std::unordered_set<std::string> set_string_0 = {"a", "b", "c", "d", "e"};
+    std::unordered_set<std::string> set_string_1 = {"c", "d", "e", "f", "g"};
+
+    utility::update_union_set(&set_int_0, set_int_1);
+    utility::update_union_set(&set_string_0, set_string_1);
+
+    EXPECT_TRUE(set_int_0.find(1) != set_int_0.end());
+    EXPECT_TRUE(set_int_0.find(2) != set_int_0.end());
+    EXPECT_TRUE(set_int_0.find(3) != set_int_0.end());
+    EXPECT_TRUE(set_int_0.find(4) != set_int_0.end());
+    EXPECT_TRUE(set_int_0.find(5) != set_int_0.end());
+    EXPECT_TRUE(set_int_0.find(6) != set_int_0.end());
+    EXPECT_TRUE(set_int_0.find(7) != set_int_0.end());
+
+    EXPECT_TRUE(set_string_0.find("a") != set_string_0.end());
+    EXPECT_TRUE(set_string_0.find("b") != set_string_0.end());
+    EXPECT_TRUE(set_string_0.find("c") != set_string_0.end());
+    EXPECT_TRUE(set_string_0.find("d") != set_string_0.end());
+    EXPECT_TRUE(set_string_0.find("e") != set_string_0.end());
+    EXPECT_TRUE(set_string_0.find("f") != set_string_0.end());
+    EXPECT_TRUE(set_string_0.find("g") != set_string_0.end());
 }
 
 /*****************************************************************************/
@@ -160,6 +160,31 @@ TEST_F(TestUnorderedSetUtility, to_vector) {
                 vector_string.end());
     EXPECT_TRUE(std::find(vector_string.begin(), vector_string.end(), "e") !=
                 vector_string.end());
+}
+
+/*****************************************************************************/
+TEST_F(TestUnorderedSetUtility, from_vector) {
+    std::vector<int>         vector_int    = {1, 2, 2, 4, 5};
+    std::vector<std::string> vector_string = {"a", "b", "b", "d", "e"};
+
+    auto set_int    = utility::from_vector(vector_int);
+    auto set_string = utility::from_vector(vector_string);
+
+    EXPECT_TRUE(std::find(set_int.begin(), set_int.end(), 1) != set_int.end());
+    EXPECT_TRUE(std::find(set_int.begin(), set_int.end(), 2) != set_int.end());
+    EXPECT_TRUE(std::find(set_int.begin(), set_int.end(), 4) != set_int.end());
+    EXPECT_TRUE(std::find(set_int.begin(), set_int.end(), 5) != set_int.end());
+    EXPECT_EQ(4, static_cast<int>(set_int.size()));
+
+    EXPECT_TRUE(std::find(set_string.begin(), set_string.end(), "a") !=
+                set_string.end());
+    EXPECT_TRUE(std::find(set_string.begin(), set_string.end(), "b") !=
+                set_string.end());
+    EXPECT_TRUE(std::find(set_string.begin(), set_string.end(), "d") !=
+                set_string.end());
+    EXPECT_TRUE(std::find(set_string.begin(), set_string.end(), "e") !=
+                set_string.end());
+    EXPECT_EQ(4, static_cast<int>(set_string.size()));
 }
 }  // namespace
 /*****************************************************************************/
