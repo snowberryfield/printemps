6456f50f89c0acd72f335454ad5c2ffe829c53dd
snowberryfield <yuji.koguma@gmailcom>
2024-12-08 23:46:18 +0900

    Output: Round a violation value to zero if it becomes minute negative error due to a numerical error.

--- printemps/solver/tabu_search/core/tabu_search_core.h
+++ printemps/solver/tabu_search/core/tabu_search_core.h
@@ -628,7 +628,7 @@ class TabuSearchCore {
             color_current_feasible_end.c_str(),
             INCUMBENT_SOLUTION_SCORE.objective * SIGN,
             color_incumbent_feasible_begin.c_str(),
-            INCUMBENT_SOLUTION_SCORE.total_violation * SIGN,
+            std::max(INCUMBENT_SOLUTION_SCORE.total_violation, 0.0) * SIGN,
             color_incumbent_feasible_end.c_str());
     }
 
@@ -737,7 +737,7 @@ class TabuSearchCore {
             INCUMBENT_SOLUTION_SCORE.objective * SIGN,
             color_incumbent_update_end.c_str(),
             color_incumbent_feasible_begin.c_str(),
-            INCUMBENT_SOLUTION_SCORE.total_violation * SIGN,
+            std::max(INCUMBENT_SOLUTION_SCORE.total_violation, 0.0),
             color_incumbent_feasible_end.c_str());
     }
     /*************************************************************************/
