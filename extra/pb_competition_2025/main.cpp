99de33459a39893a762af682e5eb0d70ef3c3e6e
Yuji KOGUMA <yuji.koguma@gmail.com>
2025-05-18 00:40:29 +0900

    Feature: Implement an extra solver for pseudo-boolean competition 2025.

---
+++ extra/pb_competition_2025/main.cpp
@@ -0,0 +1,21 @@
+/*****************************************************************************/
+// Copyright (c) 2020-2025 Yuji KOGUMA
+// Released under the MIT license
+// https://opensource.org/licenses/mit-license.php
+/*****************************************************************************/
+#include <printemps.h>
+#include "pb_competition_2025_solver.h"
+
+int main(const int argc, const char *argv[]) {
+    try {
+        printemps::extra::pb_competition_2025::PBCompetition2025Solver(argc,
+                                                                       argv)
+            .run();
+    } catch (const std::exception &e) {
+        std::cout << "s UNSUPPORTED" << std::endl;
+    }
+    return 0;
+}
+/*****************************************************************************/
+// END
+/*****************************************************************************/
