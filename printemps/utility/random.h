8ab7a38578e320a1ab248dbf1cc0f648836142c7
Yuji KOGUMA <yuji.koguma@gmail.com>
2025-05-18 23:32:27 +0900

    Fix: Correct the interface of UniformRandom class.

--- printemps/utility/random.h
+++ printemps/utility/random.h
@@ -22,13 +22,27 @@ class UniformRandom {
 
     /*************************************************************************/
     UniformRandom(const T_Value a_MIN, const T_Value a_MAX, const int a_SEED) {
-        this->setup(a_MIN, a_MAX, static_cast<unsigned int>(a_SEED));
+        this->setup(a_MIN, a_MAX, a_SEED);
     }
 
     /*************************************************************************/
     UniformRandom(const T_Value a_MIN, const T_Value a_MAX,
                   const unsigned int a_SEED) {
         this->setup(a_MIN, a_MAX, a_SEED);
+    }
+
+    /*************************************************************************/
+    inline void setup(const T_Value a_MIN, const T_Value a_MAX,
+                      const int a_SEED) {
+        m_distribution = T_Generator(a_MIN, a_MAX);
+        m_engine.seed(static_cast<unsigned int>(a_SEED));
+    }
+
+    /*************************************************************************/
+    inline void setup(const T_Value a_MIN, const T_Value a_MAX,
+                      const unsigned int a_SEED) {
+        m_distribution = T_Generator(a_MIN, a_MAX);
+        m_engine.seed(a_SEED);
     }
 
     /*************************************************************************/
