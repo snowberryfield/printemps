33e7d4f29cfd5a61d7d6a67053e5b5dbdce46731
Masahiro Sakai <masahiro.sakai@gmail.com>
2026-05-26 09:32:01 +0900

    Extend -f option of pb_competition_2025 solver to augment negated/product variable values

--- extra/pb_competition_2025/pb_competition_2025_solver.h
+++ extra/pb_competition_2025/pb_competition_2025_solver.h
@@ -165,9 +165,10 @@ class PBCompetition2025Solver {
          * be fixed at the specified values.
          */
         if (!m_argparser.fixed_variable_file_name.empty()) {
-            const auto FIXED_VARIABLES_AND_VALUES =
+            auto FIXED_VARIABLES_AND_VALUES =
                 printemps::helper::read_names_and_values(
                     m_argparser.fixed_variable_file_name);
+            m_opb.augment_solution(FIXED_VARIABLES_AND_VALUES);
             m_model.fix_variables(FIXED_VARIABLES_AND_VALUES);
         }
 
